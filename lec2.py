f7df74dea4b790d48433178cf6d87dce21116a2b
EC2 Default User <ec2-user@ip-172-31-31-213.ec2.internal>
2021-02-08 16:57:33 +0000

    lec4

--- lec2.py
+++ lec2.py
@@ -8,24 +8,4 @@ print( type('123') )
 
 print('It"s our second python class')
 
-print('Hello' + ' World')
-
-my_str = 'hello world'
-
-print(my_str)
-
-my_str = 'second str'
-
-print(my_str)
-
-my_int = 2
-
-my_float = 2.0
-
-print(my_int + 3)
-
-print(my_int * 3)
-
-print(my_int ** 3)
-
-print(my_int + my_float)
+print('Hello, World'.split(''))
